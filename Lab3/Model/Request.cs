8139e278bcc8b60f0f7d7a6a615210f91a01d1f7
Салтанат Алимбекова <@local.itstep>
2019-02-13 17:58:03 +0600

    Add project files.

---
+++ Lab3/Model/Request.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Model
+{
+    public enum RequestType { SALES, SALESIMS, STORED, MOVING, GOODS_RECEIPT}
+    public class Request
+    {
+        public Request()
+        {
+
+        }
+        public Request(DateTime data1, DateTime data2)
+        {
+            DATA1 = data1;
+            DATA2 = data2;
+        }
+        public RequestType RequestType {get; set;}
+        /// <summary>
+        /// Код поставщика из системы SAP ERP (или BIN).
+        /// </summary>
+        public int Kunnr { get; set; }
+
+        /// <summary>
+        /// Код БЕ в SAP ERP. Код БЕ может быть заполнен, а может и не заполнен
+        /// </summary>
+        public int BUKRS { get; set; }
+        /// <summary>
+        /// Завод, в рамках которого необходимо передать данные по продажам из системы SAP ERP. 
+        /// </summary>
+        public int WERKS { get; set; }
+        /// <summary>
+        /// Дата с начало периода
+        /// </summary>
+        public DateTime DATA1 { get; set; }
+        /// <summary>
+        /// Дата конец периода
+        /// </summary>
+        public DateTime DATA2 { get; set; }
+    }
+
+}
---
+++ Lab3/Model/Salescs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Model
+{
+    public class Salescs
+    {
+        public int BUKRS { get; set; }
+        public int WERKS { get; set; }
+        public int LGORT { get; set; }
+        public int SALES_CHANEL { get; set; }
+        public string SALES_CHANEL_TXT { get; set; }
+        public int KUNNR { get; set; }
+        public string KUNNR_TXT { get; set; }
+        private string ID_OUT_DELIVERY_DATA_ { get; set; }
+        public string ID_OUT_DELIVERY_DATA
+        {
+            get
+            {
+                return ID_OUT_DELIVERY_DATA_;
+            }
+            set
+            {
+
+                DateTime tmp;
+                if (DateTime.TryParse(value, out tmp))
+                {
+                    ID_OUT_DELIVERY_DATA_ = string.Format("{0:dd-mm-yy}", tmp);
+                }
+                else
+                    ID_OUT_DELIVERY_DATA_ = "";
+                ID_OUT_DELIVERY_DATA_ = value;
+            }
+        }
+    }
+}
